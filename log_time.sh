413f95b90263d0291ef5d6fdaa72a8be8e5a8ccf
haz <hazpoldev@gmail.com>
2025-04-14 00:34:22 +0800

    updated log_time

--- log_time.sh
+++ log_time.sh
@@ -1,3 +1,14 @@
-# shellcheck disable=SC2148
+#!/bin/bash
 
-echo "Current Date and Time: $(date)" >> log.txt
+current_time=$(date "+%Y-%m-%d %H:%M:%S")
+github_username=$GITHUB_ACTOR
+
+if [ ! -f log.txt ]; then
+  echo "Log file created at $current_time by $github_username" > log.txt
+else
+  update_count=$(grep -c 'Logged by' log.txt)
+  echo "Log update #$update_count: $current_time by $github_username" > log.txt
+fi
+
+echo "Logged by: $github_username at $current_time" >> log.txt
+echo "The log has been updated $update_count times." >> log.txt
